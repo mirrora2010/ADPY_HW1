024d95517bf5b9b2aea1ea8cacc06fa334e3c86b
mirrora2010 <63370321+mirrora2010@users.noreply.github.com>
2020-08-27 20:51:22 +0200

    add homework 1

---
+++ application/db/people.py
@@ -0,0 +1,3 @@
+def get_employees():
+    print('Это список сотрудников, которые получат деньги.')
+    return None
---
+++ application/salary.py
@@ -0,0 +1,3 @@
+def calculate_salary():
+    print('Сейчас начислим вам зарплатку.')
+    return None
